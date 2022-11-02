b62edc0333b9f69a12c84269d9ac066007f4edd5
Jean Paul <yjeanpaul@gmail.com>
2022-11-02 12:12:02 -0500

    Added comment to add styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Form, App, AsyncForm } from "./pages";
-// import "./index.css";
+
+// Descomentar para poder ver los estilos de App correctamente
+// import "./styles/index.css";
+// import './styles/App.css';
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Form />
+    <App />
 );
